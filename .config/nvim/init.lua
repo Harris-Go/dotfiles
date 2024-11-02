fc8d92f6a8cfe0042aa59c530eeba6558abe701f
Harris-Go <harrisgoodman1@gmail.com>
2024-11-02 19:41:25 +0000

    Changed to init.lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,42 @@
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+Plug('sainnhe/everforest')
+Plug('nvim-lualine/lualine.nvim')
+Plug('nvim-tree/nvim-web-devicons')
+vim.call('plug#end')
+
+-- Colourscheme Settings
+vim.g.everforest_background = 'hard'
+vim.o.background = 'dark'
+vim.g.everforest_better_performance = 1
+vim.g.everforest_transparent_background = 2
+vim.g.everforest_ui_contrast = 'high'
+vim.cmd([[colorscheme everforest]])
+
+-- Lualine Settings
+require('lualine').setup {
+	options = { theme = 'everforest' }
+}
+
+vim.o.termguicolors = true		-- Enable RGB Colours	
+vim.o.number = true			-- Enable line numbers
+vim.o.relativenumber = true		-- Enable relative line numbers
+vim.o.swapfile = false			-- Disable swap file
+vim.o.smartindent = true		-- Auto indent new lines
+vim.o.cursorline = true			-- Highlight line
+vim.o.wrap = false			-- Disable line wrap
+vim.o.hlsearch = true			-- Highlight search results
+vim.o.incsearch = true			-- Highlight incrementally
+
+vim.cmd('syntax enable')		-- Syntax highlighting
+vim.cmd('filetype plugin indent on') 
+
+-- Leader Key
+vim.g.mapleader = ';'
+
+-- Escape clears highlighting
+vim.api.nvim_set_keymap('n', '<Esc>', ':noh<CR>', { noremap = true, silent = true})
+
+-- Leader s sources the file
+vim.api.nvim_set_keymap('n', '<leader>s', ':source<CR>', {noremap = true, silent = true})
