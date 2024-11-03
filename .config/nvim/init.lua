356893d2e05b24f02349fae8016ddc0f9ea183d5
Harris-Go <harrisgoodman1@gmail.com>
2024-11-03 11:58:15 +0000

    Added treesitter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,6 +4,7 @@ vim.call('plug#begin')
 Plug('sainnhe/everforest')
 Plug('nvim-lualine/lualine.nvim')
 Plug('nvim-tree/nvim-web-devicons')
+Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
 vim.call('plug#end')
 
 -- Colourscheme Settings
@@ -14,10 +15,6 @@ vim.g.everforest_transparent_background = 2
 vim.g.everforest_ui_contrast = 'high'
 vim.cmd([[colorscheme everforest]])
 
--- Lualine Settings
-require('lualine').setup {
-	options = { theme = 'everforest' }
-}
 
 vim.o.termguicolors = true		-- Enable RGB Colours	
 vim.o.number = true			-- Enable line numbers
@@ -29,14 +26,21 @@ vim.o.wrap = false			-- Disable line wrap
 vim.o.hlsearch = true			-- Highlight search results
 vim.o.incsearch = true			-- Highlight incrementally
 
-vim.cmd('syntax enable')		-- Syntax highlighting
-vim.cmd('filetype plugin indent on') 
-
+-- KEYBOARD REMAPS
 -- Leader Key
 vim.g.mapleader = ';'
-
 -- Escape clears highlighting
 vim.api.nvim_set_keymap('n', '<Esc>', ':noh<CR>', { noremap = true, silent = true})
-
 -- Leader s sources the file
 vim.api.nvim_set_keymap('n', '<leader>s', ':source<CR>', {noremap = true, silent = true})
+
+
+-- Treesitter Settings
+require('nvim-treesitter.configs').setup{
+	highlight = { enable = true }
+}
+
+-- Lualine Settings
+require('lualine').setup {
+	options = { theme = 'everforest' }
+}
